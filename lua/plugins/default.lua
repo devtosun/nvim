42db0a6848d0a070283c350a409ef82bc9680d0c
Muhammed Tosun <tosun.muhammed@outlook.com>
2024-05-12 02:14:03 +0300

    first commit

--- lua/plugins/default.lua
+++ lua/plugins/default.lua
@@ -60,8 +60,3 @@ vim.cmd([[autocmd InsertLeave * lua vim.cmd(':silent! write')]])
 -- Normal moddayken dosyadan ayrıldığınızda otomatik kaydetme
 vim.cmd([[autocmd BufLeave * lua vim.cmd(':silent! write')]])
 
--- vim.cmd([[autocmd BufChange * lua vim.cmd(':silent! write')]])
-
-
-vim.api.nvim_buf_set_keymap(0, "n", "<leader>ca", "<cmd>lua vim.api.nvim_buf_set_completion({ auto_complete = true, complete_opt = { complete_behavior = vim.api.nvim_complete_behavior.COMPLETE_OPT_COMPLETE } })<cr>", {})
-vim.api.nvim_buf_set_keymap(0, "n", "<leader>cc", "<cmd>lua vim.api.nvim_buf_set_completion({ auto_complete = true })<cr>", {})
